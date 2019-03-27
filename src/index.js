16986c32f2e194a0c5bfaf9656c26c4840914773
Kevin Kredit <kevin@KK-0.localdomain>
2019-03-26 23:51:06 -0400

    Through step 7, though something wrong with packages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AuthorForm = (props) => {
+  return (
+    <div className="author-form">
+      Our Author Form Goes Here.
+    </div>
+  );
+}
+
+const AuthorList = (props) => {
+  return (
+    <div className="author-list">
+      Our Author List Goes Here.
+    </div>
+  );
+}
+
+class Authors extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      authors: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="authors">
+        <AuthorForm />
+        <AuthorList />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Authors />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
