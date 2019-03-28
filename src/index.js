775717b116a00098c6eb5314bd253eeda9d2d9ac
Kevin Kredit <k.kredit.us@ieee.org>
2019-03-28 18:39:20 -0400

    Done with part 1

--- src/index.js
+++ src/index.js
@@ -14,10 +14,54 @@ const AuthorForm = (props) => {
 }
 
 const AuthorList = (props) => {
+	const authorItems = props.authors.map((author) => {
+    return (
+      <AuthorListItem
+        fname={author.fname}
+        lname={author.lname}
+        email={author.email}
+        id={author.id}
+        key={author.id}
+        onDelete={props.onDelete}
+        onEdit={props.onEdit}
+      />
+    )
+  });
+
   return (
     <div className="author-list">
-      Our Author List Goes Here.
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th className="col-md-3">First Name</th>
+            <th className="col-md-3">Last Name</th>
+            <th className="col-md-3">Email</th>
+            <th className="col-md-3">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {authorItems}
+        </tbody>
+      </table>
     </div>
+  );
+}
+
+const AuthorListItem = (props) => {
+  return (
+    <tr>
+      <td className="col-md-3">{props.fname}</td>
+      <td className="col-md-3">{props.lname}</td>
+      <td className="col-md-3">{props.email}</td>
+      <td className="col-md-3 btn-toolbar">
+        <button className="btn btn-success btn-sm" onClick={event => props.onEdit("edit",props)}>
+          <i className="glyphicon glyphicon-pencil"></i> Edit
+        </button>
+        <button className="btn btn-danger btn-sm" onClick={event => props.onDelete(props.id)}>
+          <i className="glyphicon glyphicon-remove"></i> Delete
+        </button>
+      </td>
+    </tr>
   );
 }
 
@@ -28,14 +72,31 @@ class Authors extends React.Component {
     this.state = {
       authors: [],
     };
+    this.loadAuthors = this.loadAuthors.bind(this);
   }
 
   render() {
     return (
       <div className="authors">
         <AuthorForm />
-        <AuthorList />
+        <AuthorList authors={this.state.authors} />
       </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log('Authors mounted!')
+    this.loadAuthors();
+  }
+
+  loadAuthors() {
+    this.setState({
+      authors: [
+        {id: 0, fname: "sam", lname: "iam", email: "sam@aol.com"},
+        {id: 1, fname: "jane", lname: "doe", email: "jane@aol.com"},
+        {id: 2, fname: "fred", lname: "bear", email: "fred@aol.com"},
+        {id: 3, fname: "ted", lname: "tooy", email: "ted@aol.com"},
+      ]}
     );
   }
 }
